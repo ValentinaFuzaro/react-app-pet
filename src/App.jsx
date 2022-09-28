4b07912c4abd3d54997a0c8d16f92eebc30dc0e2
ValentinaFuzaro <rm95741@fiap.com>
2022-09-28 16:45:03 -0300

    ajustes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PetCadastro from './Components/PetCadastro';
+
+export default function App() {
+  return (
+    <>
+        <h1>App Pet!</h1>
+        <PetCadastro/>
+    </>
+  )
+}
