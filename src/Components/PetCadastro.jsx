fc114b67f1f6ae971773ca29030b04f7ccae0b46
AraujoAlan <aalan.araujo@hotmail.com>
2022-09-27 22:39:09 -0300

    ajustes de pastas, projeto no ar.

---
+++ src/Components/PetCadastro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function PetCadastro() {
+
+    // const [petCadastro, setPetCadastro] = useState([
+    //     {
+    //       "nome" : "Tobby",
+    //       "idade" : "2 anos",
+    //       "raca":"lulu da pomerania",
+    //       "tamanho" : "30cm",
+    //       "nome do dono" : "Joaquim",
+    //       "imagem do pet":"img",
+    //       "observacoes":"Muito bagunceiro"
+    //     }
+    // ])
+
+  return (
+    <>
+        <h1>FormCadastroPet</h1>
+        <p>aqui vai o form para cadastro pet.</p>
+    </>
+  )
+}
